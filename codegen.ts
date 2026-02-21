8423bf011ffed2bd40859d1ca064a236236d1ae5
David Israwi <daisrawi@microsoft.com>
2026-02-21 00:57:03 -0800

    add codegen

---
+++ codegen.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  schema: {
+    'https://api.smash.gg/gql/alpha': {
+      headers: {
+        Authorization: `Bearer ${process.env.STARTGG_TOKEN}`,
+      },
+    },
+  },
+  documents: 'src/queries/**/*.ts',
+  generates: {
+    'src/gql/graphql.ts': {
+      plugins: ['typescript', 'typescript-operations'],
+    },
+  },
+};
+
+export default config;
