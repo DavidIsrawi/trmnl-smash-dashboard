b333ce30748d64fe03c7b4e9d7cd132a6a3a94a2
David Israwi <daisrawi@microsoft.com>
2026-02-23 23:29:39 -0800

    nicer styling for the icon + singular 'day' when 1

--- src/preview.ts
+++ src/preview.ts
@@ -6,67 +6,62 @@ import type { SmashPluginData } from "./types.js";
 const engine = new Liquid();
 
 const mockData: SmashPluginData = {
-  user: {
-    gamerTag: "Dahveed",
-    images: [
+  "user": {
+    "gamerTag": "Dahveed",
+    "images": [
       {
-        id: "2948184",
-        url: "https://images.start.gg/images/user/1114021/image-d904c0bd0ca3c74cc0242188571d3fa9.jpg?ehk=FeHLFGUXMAMW8SYyrwPAygviagn99IX2%2BWHsLN2jD0U%3D&ehkOptimized=cWpUBDh91HOjuhqZgoqR7fBnWXlASKrljCjWuspmb8A%3D",
-        type: "profile",
-      },
-    ],
+        "id": "2948184",
+        "url": "https://images.start.gg/images/user/1114021/image-d904c0bd0ca3c74cc0242188571d3fa9.jpg?ehk=FeHLFGUXMAMW8SYyrwPAygviagn99IX2%2BWHsLN2jD0U%3D&ehkOptimized=cWpUBDh91HOjuhqZgoqR7fBnWXlASKrljCjWuspmb8A%3D",
+        "type": "profile"
+      }
+    ]
   },
-  season: {
-    win_rate: 65,
-    wins: 47,
-    losses: 25,
-    top_chars: [
+  "season": {
+    "win_rate": 52,
+    "wins": 31,
+    "losses": 29,
+    "top_chars": [
       {
-        name: "King K. Rool",
-        usage_count: 66,
-        image_url:
-          "https://images.start.gg/images/character/1407/image-649a64550b8848a082dbf9b8d6e545d1.png?ehk=RHaPklJczUB21bhnLnALkwi4XQXxRlZdpJUbo1S7SMk%3D&ehkOptimized=F4qN1P5o3OPxGHmBl4MA9SfxS2zLdSrbZ2%2FRLFYoILE%3D",
+        "name": "King K. Rool",
+        "usage_count": 52,
+        "image_url": "https://images.start.gg/images/character/1407/image-649a64550b8848a082dbf9b8d6e545d1.png?ehk=RHaPklJczUB21bhnLnALkwi4XQXxRlZdpJUbo1S7SMk%3D&ehkOptimized=F4qN1P5o3OPxGHmBl4MA9SfxS2zLdSrbZ2%2FRLFYoILE%3D"
       },
       {
-        name: "R.O.B.",
-        usage_count: 6,
-        image_url:
-          "https://images.start.gg/images/character/1323/image-ff45bd739bb70c5b557247b071757f9c.png?ehk=GTLNejSa6BYTPzaBO%2FThijmCIzfj6tJ5dsZcyOBCZu8%3D&ehkOptimized=T8O79GGv3DdxUB5LDUeRNy9G6RzpR4GeLlQBWulkrFE%3D",
-      },
-    ],
+        "name": "R.O.B.",
+        "usage_count": 6,
+        "image_url": "https://images.start.gg/images/character/1323/image-ff45bd739bb70c5b557247b071757f9c.png?ehk=GTLNejSa6BYTPzaBO%2FThijmCIzfj6tJ5dsZcyOBCZu8%3D&ehkOptimized=T8O79GGv3DdxUB5LDUeRNy9G6RzpR4GeLlQBWulkrFE%3D"
+      }
+    ]
   },
-  next_tournament: {
-    name: "Genesis X",
-    days_remaining: 14,
-    image_url:
-      "https://images.start.gg/images/tournament/756975/image-0c9da33e1bcad42e0104a43f02623183.png",
+  "next_tournament": {
+    "name": "Octagon #151",
+    "days_remaining": 1,
+    "image_url": "https://images.start.gg/images/tournament/884418/image-9f7dc1280f551590aa95b8087421a1bf.png"
   },
-  latest_result: {
-    rank: 7,
-    upset_factor: 3,
-    event_name: "Ultimate Singles (ALTERNATE STAGE LIST)",
-    tournament_name: "Octagon #150",
-    date: "Feb 17",
-    location: "Seattle",
-    entrants: 46,
-    wins: 5,
-    losses: 2,
-    char_image_url:
-      "https://images.start.gg/images/character/1407/image-c8adee7aaf47b788e22ad79842aba06c.png?ehk=y33xj%2BHmrlUyx%2BaIocE0rt7gQpFEy%2FOh%2BI88dbevV0U%3D&ehkOptimized=mEtPbfhGopCXphR7tniuMF%2BXpiG10eWejj331QSrRzQ%3D",
-    char_played: "King K. Rool",
+  "latest_result": {
+    "rank": 7,
+    "upset_factor": 1,
+    "event_name": "7pm - Ultimate Singles (ALTERNATE STAGE LIST)",
+    "tournament_name": "Octagon #150",
+    "date": "Feb 17",
+    "location": "Seattle",
+    "entrants": 46,
+    "wins": 3,
+    "losses": 2,
+    "char_played": "King K. Rool",
+    "char_image_url": "https://images.start.gg/images/character/1407/image-c8adee7aaf47b788e22ad79842aba06c.png?ehk=y33xj%2BHmrlUyx%2BaIocE0rt7gQpFEy%2FOh%2BI88dbevV0U%3D&ehkOptimized=mEtPbfhGopCXphR7tniuMF%2BXpiG10eWejj331QSrRzQ%3D"
   },
-  previous_result: {
-    rank: 3,
-    upset_factor: 1,
-    event_name: "Ultimate Doubles",
-    tournament_name: "Octagon #150",
-    date: "Feb 17",
-    location: "Seattle",
-    entrants: 20,
-    wins: 3,
-    losses: 1,
-    char_played: "King K. Rool",
-  },
+  "previous_result": {
+    "rank": 3,
+    "upset_factor": 0,
+    "event_name": "6:30pm - Ultimate Doubles",
+    "tournament_name": "Octagon #150",
+    "date": "Feb 17",
+    "location": "Seattle",
+    "entrants": 20,
+    "wins": 4,
+    "losses": 2
+  }
 };
 
 async function generatePreview() {
